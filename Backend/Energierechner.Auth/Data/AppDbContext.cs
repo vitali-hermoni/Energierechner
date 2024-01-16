a4f56a86a6113d4a1d361cdba2df9e86e189ba3c
Vitali Hermoni <vitali_hermoni@nobicore.de.de>
2024-01-16 21:59:11 +0100

    add identity database

---
+++ Backend/Energierechner.Auth/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Energierechner.Auth.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Energierechner.Auth.Data;
+
+public class AppDbContext : IdentityDbContext<AppUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+
+    public DbSet<AppUser> AppUser { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+
+        base.OnModelCreating(builder);
+
+    }
+}
---
+++ Backend/Energierechner.Auth/Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Energierechner.Auth.Models;
+
+public class AppUser : IdentityUser
+{
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    public Guid IdenKey { get; set; }
+}
